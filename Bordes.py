113d5294153d574dd5439ecd280f90f49195fb1d
franmir <franmiranda@udec.cl>
2016-06-05 19:58:55 -0300

    Update and rename lab3.py to Bordes.py
    Utilizando imagen guardada

--- Bordes.py
+++ Bordes.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 #import the necessary packages
+
+
 from sklearn.cluster import KMeans
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-import cv2
-
-from SimpleCV import Image,Camera,Display,Image
+from SimpleCV import Image,Camera,Display,Image,Color
 
 
 img=Image("lunarr2.jpg")#importamos la imagen a la cual deseamos determinarle los bordes
+foto=img.show()
+
+
+
 
 #imagen en escala de grises
 imgGray=img.grayscale()
@@ -47,13 +51,30 @@ red.save("imagen_r.png")
 green.save("imagen_g.png")
 blue.save("imagen_b.png")
 
-imgb=imgGray.binarize(50)
+imgb=imgGray.binarize(50)  #dependiendo de la oscuridad de la imagen se modifica 
 imgb.save("bingray.png")
 
-#estimación del gradiente"
+
+#estimación del gradiente
 
 bingrad=imgb.morphGradient().save("bingradiennre.png")# determinamos el gradiente de la imagen binarizada, lo que nos da los bordes.
+fda=imgb.morphGradient()
+fotobordesgrad=fda.show()
 graygrad=imgGray.morphGradient().save("Graygradiennre.png")
 REDgrad=red.morphGradient().save("redgradiente.png")
 GREENgrad=green.morphGradient().save("greengradiente.png")
-BLUEgrad=blue.morphGradient().save("blegradiente.png")
+BLUEgrad=blue.morphGradient().save("bluegradiente.png")
+
+imgbinsobel = imgb.sobel().save("binsobel.png")
+sbl=imgb.sobel()
+imgGraysobel = imgGray.sobel().save("GRAYsobel.png")
+red_edgesobel = red.sobel().save("REDsobel.png")
+green_edgesobel = green.sobel().save("GREENsobel.png")
+blue_edgesobel = blue.sobel().save("BLUEsobel.jpg")
+
+imagenfin= fda + img
+imagenfin.save("BordesGrad.png")
+fotobordesfinal=imagenfin.show()
+
+imagenfin2= sbl+img
+imagenfin.save("BordesSobel.png")
